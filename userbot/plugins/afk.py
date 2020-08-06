32673f8d0848cd4a4a3bf15491aef25e430a9e6d
SKS <68851382+sipakisking@users.noreply.github.com>
2020-08-06 10:06:20 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -24,7 +24,7 @@ async def set_not_afk(event):
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PLUGIN_CHANNEL,  # pylint:disable=E0602
-                "Mine Owner has gone for some Important work he is very busy😅"
+                "My Owner has gone for some Important work he is very busy😅"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             await borg.send_message(  # pylint:disable=E0602
@@ -61,9 +61,9 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await event.edit(f"My Boss is going!, and Reason is {reason}")
+            await event.edit(f"My Boss is going!away from keyboard, and Reason is {reason}")
         else:
-            await event.edit(f"My Boss is Going")
+            await event.edit(f"My Boss is Going. Bye")
         await asyncio.sleep(5)
         await event.delete()
         try:
@@ -122,9 +122,9 @@ async def on_afk(event):
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
         message_to_reply = f"My Master Has Been Gone For {afk_since}\nWhere He Is: **Only God Knows** " + \
-            f"\n\n__ I'll back in a few Light years__\n**REASON**: {reason}" \
+            f"\n\n__ I'll back in a few Billion Light years__\n**REASON**: {reason}" \
             if reason \
-            else f"**Important Notice**\n\n[This User Is Ded Forever...](https://telegra.ph/file/a4821748db331a0c899a0.mp4) "
+            else f"**Important Notice**\n\n[This User Is Dead Forever...](https://telegra.ph/file/a4821748db331a0c899a0.mp4) "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
