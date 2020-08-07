6a751380a29965d43ba199c4b226c8be7e8c6f55
SKS <68851382+sipakisking@users.noreply.github.com>
2020-08-07 09:56:15 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -125,6 +125,11 @@ if bool(ENV):
 
     # Clean Welcome
     CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
+    
+     # Custom Module
+    CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
+    CUSTOM_STICKER_PACK_NAME = os.environ.get("CUSTOM_STICKER_PACK_NAME", None)
+    CUSTOM_ANIMATED_PACK_NAME = os.environ.get("CUSTOM_ANIMATED_PACK_NAME", None)
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
