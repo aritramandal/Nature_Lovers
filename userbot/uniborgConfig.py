88da53d5ec3b88ec79f1930ab55e474dabd7319f
SKS <68851382+sipakisking@users.noreply.github.com>
2020-08-07 09:56:36 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -13,7 +13,7 @@ if ENV:
         # Send .get_id in any group to fill this value.
         SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", None)
         
-        # This is required for the plugins involving the file system.
+        # This is required for the plugins involving the file system..
         TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
         # This is required for the speech to text module. Get your USERNAME from https://console.bluemix.net/docs/services/speech-to-text/getting-started.html
         IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
